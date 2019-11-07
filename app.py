0225570a4a2933cb21092629a84729e1c29a13e5
Thanuja Tekkali <tanuja1597@gmail.com>
2019-11-07 13:22:59 +0530

    Added posts fetching and post writing functionality

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy 
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://mgszgjtfvffrgz:06ddfdd12980f5848f3d0d30ea0a20a52d773371412cd10ab9a1c235e209ac36@ec2-54-217-225-16.eu-west-1.compute.amazonaws.com:5432/dfhb33b1aiskat"
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+    post_text = db.Column(db.String(1000))
+
+@app.route("/")
+def home():
+    posts = Post.query.all()
+    return render_template('home.html', posts=posts)
+
+@app.route("/newpost")
+def newpost():
+    return render_template('newpost.html')
+
+@app.route('/newpost', methods=['POST'])
+def newpostSubmit():
+    name = request.form.get('name')
+    post = request.form.get('post')
+
+    new_post = Post(name=name, post_text=post)
+    db.session.add(new_post)
+    db.session.commit()
+    
+    return redirect(url_for('home'))
